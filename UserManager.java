2373daca4aefb23e3feb3b10ba4c240e6ead9851
TryingToVibe <103157069+TryingToVibe@users.noreply.github.com>
2026-03-29 21:38:16 -0500

    Update UserManager.java

--- UserManager.java
+++ UserManager.java
@@ -11,17 +11,34 @@ public class UserManager {
 	//New user registration.
 	public static void registerUser(String u, String p) throws IOException{
 		
+		File file = new File(FILE_NAME);
+        String hashed = PasswordUtil.hashPass(p);
+        Role role;
+        
+        if (!file.exists() || file.length() == 0) {
+
+            role = Role.ADMIN;
+            System.out.println("Creating first ADMIN account.");
+
+        } else {
+        	if (!Session.isAdmin()) {
+                System.out.println("Access denied. Only admins can create users.");
+                return;
+            }
+
+            role = Role.GUEST;
+        }
+		
 		//Hashing our password using our PasswordUtil class and storing the hashed password
 		//Plain password is NEVER stored for security reasons.
-		String hashed = PasswordUtil.hashPass(p);
 		
 		FileWriter fw = new FileWriter(FILE_NAME, true);
-		fw.write(u + ":" + hashed + "\n");
+		fw.write(u + ":" + hashed + ":" + role + "\n");
 		fw.close();
 	}
 	
 	//User Authentication
-	public static boolean authenticate(String u, String p) throws IOException{
+	public static User authenticate(String u, String p) throws IOException{
 		
 		//comparing hashedInput vs storedHashed to authenticate.
 		String hashedInput = PasswordUtil.hashPass(p);
@@ -35,17 +52,18 @@ public class UserManager {
 			String[]parts = line.split(":");
 			String storedUser = parts[0];
 			String storedHash = parts[1];
+			Role role = Role.valueOf(parts[2]);
 			
 			//if both user name and password match, the login is successful,
 			//and the user has been authenticated
 			if (storedUser.equals(u) && storedHash.equals(hashedInput)) {
 				br.close();
-				return true;
+				return new User(storedUser, storedHash, role);
 			}
 		}
 		
 		br.close();
-		return false;
+		return null;
 	}
 	
 	
